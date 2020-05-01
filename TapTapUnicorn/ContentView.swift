38210ca7b758744044157a332c2685487655e239
Benjamin Dirgo <bdirgo@gmail.com>
2020-05-01 13:06:00 -0700

    Initial Commit

---
+++ TapTapUnicorn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapTapUnicorn
+//
+//  Created by Benjamin Dirgo on 5/1/20.
+//  Copyright © 2020 Benjamin Dirgo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
